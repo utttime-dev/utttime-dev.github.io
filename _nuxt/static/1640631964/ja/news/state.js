af374135804ef78436c468612e59bcc15ac93325
m12watanabe1a <m12watanabe1a@users.noreply.github.com>
2022-01-01 05:17:55 +0000

    deploy: utttime-dev/ttime-web2@4dba9204cac3b5b0b3b2dd54ae5b6b62e489bfdd

--- _nuxt/static/1640631964/ja/news/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640631964",layout:"default",error:a,serverRendered:true,routePath:"\u002Fja\u002Fnews",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dd44518d"}},__i18n:{langs:{}}}}(null));
---
+++ _nuxt/static/1641014266/links/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641014266",layout:"default",error:a,serverRendered:true,routePath:"\u002Flinks",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6342383d"}},__i18n:{langs:{}}}}(null));
