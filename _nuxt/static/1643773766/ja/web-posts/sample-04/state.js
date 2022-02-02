a9df08d27dcbd4709905dbd07e62f8e0bf090179
m12watanabe1a <m12watanabe1a@users.noreply.github.com>
2022-02-02 03:49:57 +0000

    deploy: utttime-dev/ttime-web2@4335b39d1da417536810e8eda475790c6a3fa6ab

---
+++ _nuxt/static/1643773766/ja/web-posts/sample-04/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643773766",layout:"default",error:a,serverRendered:true,routePath:"\u002Fja\u002Fweb-posts\u002Fsample-04",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c77f071f"}},__i18n:{langs:{}}}}(null));
